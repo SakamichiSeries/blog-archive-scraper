c408446d8f7fba9f91bddb00fe55841e8eda3106
GitHub Action <action@github.com>
2025-04-01 18:55:03 +0800

    Test

--- utils.py
+++ utils.py
@@ -26,9 +26,9 @@ def download_image_return_path(img_src_url: str, repo_name: str, group: str) -> 
         return img_src_url
     img_full_url = add_host(img_src_url, group)
     img_relative_path = f"{repo_name}/{urllib.parse.urlparse(img_full_url).path[1:]}"
-    if os.path.exists(img_relative_path):
-        print(f"File exists: {img_relative_path}")
-        return "/" + img_relative_path
+    # if os.path.exists(img_relative_path):
+    #     print(f"File exists: {img_relative_path}")
+    #     return "/" + img_relative_path
     os.makedirs(os.path.dirname(img_relative_path), exist_ok=True)
     fail_count = 0
     while True:
